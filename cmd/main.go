ca727de2b75b073b9228da8e3892e911eea17f7c
cdx1124 <cdx1124dp@gmail.com>
2023-02-12 14:45:55 +0800

    跨域配置

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,9 @@
 package main
 
 import (
+	"time"
+
+	"github.com/hertz-contrib/cors"
 	"github.com/okatu-loli/TikTokLite/cmd/dal"
 	"github.com/okatu-loli/TikTokLite/cmd/router"
 	"github.com/okatu-loli/TikTokLite/internal/middleware"
@@ -16,6 +19,17 @@ type Text struct {
 
 func main() {
 	h := server.Default()
+	h.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"https://foo.com"},
+		AllowMethods:     []string{"PUT", "PATCH"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return origin == "https://github.com"
+		},
+		MaxAge: 12 * time.Hour,
+	}))
 	middleware.InitJwt()
 	router.CustomizedRegister(h)
 	dal.Init()
