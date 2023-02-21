0cbeecac69ee2b107deb37818dcbe59a0aac8f96
cdx1124 <cdx1124dp@gmail.com>
2023-02-21 15:17:41 +0800

    redis的集成

--- cmd/dal/init.go
+++ cmd/dal/init.go
@@ -1,7 +1,11 @@
 package dal
 
-import "github.com/okatu-loli/TikTokLite/cmd/dal/db"
+import (
+	"github.com/okatu-loli/TikTokLite/cmd/dal/db"
+	"github.com/okatu-loli/TikTokLite/cmd/dal/rdb"
+)
 
 func Init() {
 	db.Init()
+	rdb.Init()
 }
